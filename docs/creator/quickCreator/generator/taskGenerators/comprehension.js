517b2a5f9d1177fefa3406d9d79f151dcf9e162f
xRoyBatty <80215357+xRoyBatty@users.noreply.github.com>
2024-12-07 01:15:54 +0100

    Update comprehension task generator to properly generate true/false statements

--- docs/creator/quickCreator/generator/taskGenerators/comprehension.js
+++ docs/creator/quickCreator/generator/taskGenerators/comprehension.js
@@ -1,9 +1,22 @@
 export function generateComprehensionTask(taskData) {
     return {
         ...taskData,
+        // Generate proper true/false list items
         statements: taskData.statements
-            .map(statement => `
-                -   ${statement.text}
+            .map((statement, index) => `
+                <li class="true-false-item" data-statement-index="${index}">
+                    <span class="true-false-statement">${statement.text}</span>
+                    <div class="true-false-options">
+                        <label>
+                            <input type="radio" name="statement_${index}" value="true">
+                            True
+                        </label>
+                        <label>
+                            <input type="radio" name="statement_${index}" value="false">
+                            False
+                        </label>
+                    </div>
+                </li>
             `)
             .join('\n')
     };
